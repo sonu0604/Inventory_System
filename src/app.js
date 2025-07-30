947bebc7fe846b88cfef8bf2e1162caff4a3541c
Shashank0604 <101591494+sonu0604@users.noreply.github.com>
2025-07-30 15:27:26 +0530

    changing the Api
    change

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ let express = require('express');
 let bodyparser = require('body-parser');
 let db = require("../db.js");
 let router=require('../src/routes/route.js')
+
 require("dotenv").config();
 
 let app = express();
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/', router);
